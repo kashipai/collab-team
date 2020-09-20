ef0b15a455b1a6ad9998628f6d1c98df711b9036
soumya-shree <71283141+soumya-shree@users.noreply.github.com>
2020-09-20 21:40:19 +0530

    Update ThreadOpration.java

--- ThreadOpration.java
+++ ThreadOpration.java
@@ -1,6 +1,6 @@
 package assignment_18_9;
 
-public class ThreadOpration extends Thread {
+public class ThreadOperation extends Thread {
 	static int value = 0;
 
 	public void run() {
@@ -15,13 +15,13 @@ public class ThreadOpration extends Thread {
 	public static void main(String args[]) throws Exception {
 
 		try {
-			ThreadOpration daemonThread = new ThreadOpration();
+			ThreadOperation daemonThread = new ThreadOperation();
 			daemonThread.setDaemon(true);
 			Thread.sleep(1000);
 			daemonThread.start();
-			ThreadOpration[] thread = new ThreadOpration[50];
+			ThreadOperation[] thread = new ThreadOperation[50];
 			for (int i = 0; i < 50; i++) {
-				thread[i] = new ThreadOpration();
+				thread[i] = new ThreadOperation();
 				thread[i].start();
 				Thread.sleep(1000);
 			}

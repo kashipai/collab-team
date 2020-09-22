66c7ea22065bc15ee15a582f40bf6152e98350f0
sneha-y-v <sneha.yv@robosoft.com>
2020-09-22 08:07:44 +0530

    .

--- SocketClient.java
+++ SocketClient.java
@@ -1,16 +1,16 @@
 package trainingAssignment;
 
-import java.io.DataOutputStream;
+import java.io.DataInputStream;
 import java.net.*;
 
 public class SocketClient {
 	public static void main(String[] args) {
 		try{      
 			Socket s=new Socket("localhost",6666);  
-			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
-			dout.writeUTF("Hello Server");  
-			dout.flush();  
-			dout.close();  
+			DataInputStream dis=new DataInputStream(s.getInputStream());  
+			String  str=(String)dis.readUTF();  
+			System.out.println("message= "+str); 
+			dis.close();  
 			s.close();  
 			}catch(Exception e){System.out.println(e);}  
 	}
--- SocketServer.java
+++ SocketServer.java
@@ -1,17 +1,19 @@
 package trainingAssignment;
 
-import java.io.DataInputStream;
+import java.io.DataOutputStream;
 import java.net.*;
 
 public class SocketServer {
 	public static void main(String[] args) {
 		try{  
 			ServerSocket ss=new ServerSocket(6666);  
-			Socket s=ss.accept();//establishes connection   
-			DataInputStream dis=new DataInputStream(s.getInputStream());  
-			String  str=(String)dis.readUTF();  
-			System.out.println("message= "+str);  
-			ss.close();  
+			Socket s=ss.accept();//establishes connection     
+			DataOutputStream dout=new DataOutputStream(s.getOutputStream());  
+			dout.writeUTF("Hello Server");  
+			dout.flush();  
+			dout.close();  
+			s.close();  
 			}catch(Exception e){System.out.println(e);}  
 	}
 }
+

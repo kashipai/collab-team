40de35b401cdbb93827206bad10f985ec5b84156
sneha-y-v <sneha.yv@robosoft.com>
2020-09-28 12:33:34 +0530

    synchronized counter increment and sorting employee data.

--- ThreadSynchronization.java
+++ ThreadSynchronization.java
@@ -3,7 +3,8 @@ import java.util.concurrent.*;
 import java.util.concurrent.locks.ReentrantLock;
 
 class ThreadCounter extends Thread{
-	    static int count = 1; 
+	    static int count = 1;
+	  
 	    private static Object lock = new Object(); 	   
 		public  void run() {
 			
@@ -11,43 +12,7 @@ class ThreadCounter extends Thread{
 				 System.out.println(Thread.currentThread().getName() + " : " + count);
 		            count++;
 			 }
-			
-			/* 
-			 private ReentrantLock lock;
-			 public ThreadCounter(ReentrantLock lock) {
-			// TODO Auto-generated constructor stub
-	    	this.lock=lock;
-		     }
-			lock.lock();
-	        try {
-	            System.out.println(Thread.currentThread().getName() + " : " + count);
-	            count++;
-	        }catch (Exception e) {
-				// TODO: handle exception
-	        	System.out.println(e);
-			} finally {
-	            lock.unlock();
-	        }*/
-	        
-			/* 
-			 try {
-				if(Thread.holdsLock(this)) {
-					count = count + 1;
-					System.out.println(Thread.currentThread().getName()+"\t"+count);
-					this.notifyAll();
-					
-				}
-				else {
-					wait();
-				}
-				}catch (Exception e) {
-					// TODO: handle exception
-		        	System.out.println(e);
-				} 			
-				
-		     */
-	
-	}
+		}			
 }	
 public class ThreadSynchronization {
 

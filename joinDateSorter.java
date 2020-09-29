1d28959451fa79e5b85d95966b49996747b50e8a
Vaishnavi-RBhat <71436888+Vaishnavi-RBhat@users.noreply.github.com>
2020-09-29 13:47:40 +0530

    Update joinDateSorter.java

--- joinDateSorter.java
+++ joinDateSorter.java
@@ -4,6 +4,6 @@ import java.util.Comparator;
 
 public class joinDateSorter implements Comparator<Employee2>  {
 	public int compare(Employee2 o1, Employee2 o2) {
-        return o1.getLastName().compareTo(o2.getLastName());
+        return o1.getDate().compareTo(o2.getDate());
     }
 }

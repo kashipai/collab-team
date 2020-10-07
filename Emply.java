deb043810c6b571323149a15786e42d009bf06c2
HarsharajDevadiga <harsharajdevadiga@gmail.com>
2020-10-07 20:21:46 +0530

    Resource bundle and JNDI

--- Emply.java
+++ Emply.java
@@ -1,65 +1,63 @@
-package Began;
+package began;
 
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
+import javax.naming.Context;
+import javax.naming.InitialContext;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.sql.DataSource;
 
-@WebServlet("/Employeee")
-public class Employeee extends HttpServlet {
+@WebServlet("/Emply")
+public class Emply extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
-	public Employeee() {
+	public Emply() {
 		super();
+
 	}
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		
-		final String JDBC_DRIVER = "com.mysql.jdbc.Driver";
-		final String DB_URL = "jdbc:mysql://localhost:3306/database2";
-		final String USER = "root";
-		final String PASS = "naruto#420";
-		String urlId = (null);
-		urlId = request.getParameter("EmpId");
-		response.setContentType("text/html");
-		PrintWriter out = response.getWriter();
-		String title = "Employee Record of EmpId " + urlId;
-		if (urlId == null) {
-			title = "Employee Record ";
-		}
 		Connection conn = null;
 		Statement stmt = null;
-		String docType = "<!doctype html>\n";
+		Context envContext = null;
+		ResourceBundle bundle = ResourceBundle.getBundle("Message", Locale.US);
 
+		Locale.setDefault(new Locale("gr", "GR"));
+		bundle = ResourceBundle.getBundle("Message");
+
+		PrintWriter out = response.getWriter();
+		String title = bundle.getString("title");
+
+		String docType = "<!doctype html>\n";
 		out.println(docType + "<html>\n" + "<head><title>" + title + "</title></head>\n"
 				+ "<body bgcolor = \"#f0f0f0\">\n" + "<h1 align = \"center\">" + title + "</h1>\n");
 		try {
+			envContext = new InitialContext();
+			Context initContext = (Context) envContext.lookup("java:/comp/env");
+			DataSource ds = (DataSource) initContext.lookup("jdbc/database2");
 
-			Class.forName(JDBC_DRIVER);
+			Connection con = ds.getConnection();
 
-			conn = DriverManager.getConnection(DB_URL, USER, PASS);
+			Statement stmt1 = con.createStatement();
+			String query = "SELECT * FROM database2.emptable";
+			ResultSet rs = stmt1.executeQuery(query);
 
-			stmt = conn.createStatement();
-			String sql;
-			sql = "SELECT * FROM database2.emptable where EmpId ='" + urlId + "'";
-			if (urlId == null) {
-				sql = "SELECT * FROM database2.emptable";
-			}
-			ResultSet rs = stmt.executeQuery(sql);
+			response.setContentType("text/html");
 			out.println("<table border=1 width=50% height=50%>");
 			out.println(
 					"<tr><th>Employee Id</th><th>First Name</th><th>Last Name</th><th>Salaray</th><th>Age</th><tr>");
-
 			while (rs.next()) {
 
 				int id = rs.getInt("EmpId");
@@ -96,6 +94,7 @@ public class Employeee extends HttpServlet {
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response)
 			throws ServletException, IOException {
-		doGet(request,response);
-}
+		doGet(request, response);
+	}
+
 }

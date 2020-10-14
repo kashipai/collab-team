3655504417ddac90c8985ce44fdeb13bdef3748c
Soumya Shree A P <soumyasap4@gmail.com>
2020-10-14 14:24:50 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,33 @@
+package exercise1;
+
+public class Student {
+	private int rollNo;  
+	private String name;  
+	private Address address;
+	private String stream;
+	    
+	
+	public Student(int rollNo, String name, Address address) {  
+	    super();  
+	    this.rollNo = rollNo;  
+	    this.name = name;  
+	    this.address = address;  
+	}  
+	public String getStream() {
+		return stream;
+	}
+
+	public void setStream(String stream) {
+		this.stream = stream;
+	}
+	  
+	void display(){ 
+		
+	    System.out.println("Roll number: "+rollNo+"\nName: "+name);  
+	    System.out.println("Stream opted: "+getStream());
+	    System.out.println(address.toString());  
+
+	}  
+	
+
+}

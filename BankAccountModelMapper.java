9781236ed8cbf2937222917e469554de4248939b
sneha-y-v <sneha.yv@robosoft.com>
2020-10-19 13:23:45 +0530

    Spring JDBC template

---
+++ BankAccountModelMapper.java
@@ -0,0 +1,19 @@
+package sneha.springJDBCTemplate;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+
+
+public class BankAccountModelMapper implements org.springframework.jdbc.core.RowMapper<BankAccount>{
+
+	public BankAccount mapRow(ResultSet resultSet, int arg1) throws SQLException {
+	
+		BankAccount bankAccount = new BankAccount() ;
+		bankAccount.setAccountId(resultSet.getString(1));
+		bankAccount.setName(resultSet.getString(2));
+
+		return bankAccount;
+	}
+
+}
---
+++ BankDAOImplementation.java
@@ -0,0 +1,48 @@
+package sneha.springJDBCTemplate;
+
+import java.util.List;
+
+import javax.sql.DataSource;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+@Component
+public class BankDAOImplementation implements BankDAO{
+	JdbcTemplate jdbcTemplate;
+	private final String SQL_FIND_ACCOUNT = "select * from bankAccount where accountId = ? ";
+	private final String SQL_UPDATE_ACCOUNT = "update bankAccount set name = ?  where accountId = ? ";
+	private final String SQL_GET_ALL = "select * from bankAccount";
+	private final String SQL_DELETE_ACCOUNT = "delete from bankAccount where accountId = ? ";
+	private final String SQL_CREATE_ACCOUNT = "insert into bankAccount (accountId, name) values (?,?)";
+
+	
+	public BankDAOImplementation(DataSource dataSource) {
+		jdbcTemplate = new JdbcTemplate(dataSource);
+	}
+
+	public BankAccount getBankAccountById(String accountId) {
+		// TODO Auto-generated method stub
+		return jdbcTemplate.queryForObject(SQL_FIND_ACCOUNT, new Object[] {accountId} ,new BankAccountModelMapper());
+	}
+
+	public List<BankAccount> getAllAccountHolder() {
+		// TODO Auto-generated method stub
+		return jdbcTemplate.query(SQL_GET_ALL, new BankAccountModelMapper());
+	}
+
+	public boolean deleteAccount(String accountId) {
+		// TODO Auto-generated method stub
+		return jdbcTemplate.update(SQL_DELETE_ACCOUNT,accountId)>0;
+	}
+
+	public boolean updateAccount(BankAccount bankAccount) {
+		// TODO Auto-generated method stub
+
+		return jdbcTemplate.update(SQL_UPDATE_ACCOUNT,bankAccount.getName(),bankAccount.getAccountId())>0;
+	}
+
+	public boolean createAccount(String accountId,String name) {
+		// TODO Auto-generated method stub
+		return jdbcTemplate.update(SQL_CREATE_ACCOUNT,accountId ,name)>0;
+	}
+}

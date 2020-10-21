f393aed94f5e67e14054cd3c415da7693e02a5c8
Soumya Shree A P <soumyasap4@gmail.com>
2020-10-21 13:45:47 +0530

    Create Store.java

---
+++ Store.java
@@ -0,0 +1,53 @@
+package exercise1610;
+
+public class Store {
+	private int productId;  
+	private String productName;  
+	private int price;
+	private int quantity;
+	
+
+	public Store() {
+		// TODO Auto-generated constructor stub
+	}
+
+	
+    public Store(int personId, String productName, int price, int quantity) {
+        super();
+        this.productId = personId;
+        this.productName = productName;
+        this.price =  price;
+        this.quantity = quantity;
+    }
+
+	public int getProductId() {
+		return productId;
+	}
+	public void setProductId(int productId) {
+		this.productId = productId;
+	}
+	public String getProductName() {
+		return productName;
+	}
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	public int getQuantity() {
+		return quantity;
+	}
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+	}
+	
+	@Override
+	public String toString() {
+		return "Store [productId=" + productId + ", productName=" + productName + ", price=" + price + ", quantity="
+				+ quantity + "]";
+	}
+}

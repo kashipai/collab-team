e5d05f03f7cdda8b8e590c47dd37a24214ca6275
Soumya Shree A P <soumyasap4@gmail.com>
2020-10-29 12:53:39 +0530

    Create Author.java

---
+++ jpa_entity_Exercise/Author.java
@@ -0,0 +1,63 @@
+package jpa_entity_Exercise;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table(name = "Author")
+public class Author {
+
+	@Id
+	@Column(name = "authorId")
+    @GeneratedValue
+    private int authorId;
+    private String firstName;
+    private String lastName;
+    
+	public Author() {
+	}
+
+	public Author(int id, String firstName, String lastName) {
+		super();
+		this.authorId = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public int getId() {
+		return authorId;
+	}
+
+	public void setId(int id) {
+		this.authorId = id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	@Override
+	public String toString() {
+		return "Author [id=" + authorId + ", firstName=" + firstName + ", lastName=" + lastName + "]";
+	}
+	
+	
+    
+ 
+}

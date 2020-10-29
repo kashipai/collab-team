5ae8bceb7e604ca56b6bee18b85200fce24c66dd
Soumya Shree A P <soumyasap4@gmail.com>
2020-10-29 13:06:30 +0530

    Create EmployeeRepository.java

---
+++ EmployeeRepository.java
@@ -0,0 +1,9 @@
+package com.springMVC;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
+	 List<Employee> findByLastName(String lastName);
+}

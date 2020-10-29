11d94aebf8c41d78b2a37d011dff8730b63e65fc
Soumya Shree A P <soumyasap4@gmail.com>
2020-10-29 13:01:58 +0530

    Create AuthorRepository.java

---
+++ AuthorRepository.java
@@ -0,0 +1,10 @@
+package jpa_oneToMany;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+ 
+public interface AuthorRepository extends JpaRepository<Author, Integer> {
+	 List<Author> findByLastName(String lastName);
+}

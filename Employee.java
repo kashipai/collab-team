3779f78630dfa9a799462b389208236efa5c0bdf
Soumya Shree A P <soumyasap4@gmail.com>
2020-10-29 13:06:24 +0530

    Create Employee.java

---
+++ Employee.java
@@ -0,0 +1,69 @@
+package com.springMVC;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "Employee")
+public class Employee {
+
+	@Id
+	@Column(name = "EmpId")
+	private int empId;
+	private String firstName;
+	private String lastName;
+	private int age;
+
+	public Employee() {
+
+	}
+
+	public Employee(int empId, String firstName, String lastName, int age) {
+		super();
+		this.empId = empId;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+
+	public int getEmpId() {
+		return empId;
+	}
+
+	public void setEmpId(int empId) {
+		this.empId = empId;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+	@Override
+	public String toString() {
+		return "Employee [empId=" + empId + ", firstName=" + firstName + ", lastName=" + lastName + ", age=" + age
+				+ "]";
+	}
+
+}

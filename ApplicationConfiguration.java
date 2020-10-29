c212120a632632ead4125606cc636559045dde61
Soumya Shree A P <soumyasap4@gmail.com>
2020-10-29 12:59:28 +0530

    Create ApplicationConfiguration.java

---
+++ ApplicationConfiguration.java
@@ -0,0 +1,82 @@
+package jpa_oneToOne;
+
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.dao.annotation.PersistenceExceptionTranslationPostProcessor;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@Configuration
+@ComponentScan(basePackages = "jpa_oneToOne")
+@EnableJpaRepositories(basePackages = {"jpa_oneToOne"})
+@EnableTransactionManagement
+public class ApplicationConfiguration {
+	 
+	   @Bean
+	   public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+	      LocalContainerEntityManagerFactoryBean em 
+	        = new LocalContainerEntityManagerFactoryBean();
+	      em.setDataSource(dataSource());
+	      em.setPackagesToScan(new String[] { "jpa_oneToOne" });
+	 
+	      JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+	      em.setJpaVendorAdapter(vendorAdapter);
+	      em.setJpaProperties(additionalProperties());
+	 
+	      return em;
+	   }
+	   @Bean
+	   public DataSource dataSource(){
+	       DriverManagerDataSource dataSource = new DriverManagerDataSource();
+	       dataSource.setDriverClassName("com.mysql.jdbc.Driver");
+	       dataSource.setUrl("jdbc:mysql://localhost:3306/Database4");
+	       dataSource.setUsername( "root" );
+	       dataSource.setPassword( "MySQLpassword" );
+	       return dataSource;
+	   }
+	   @Bean
+	   public PlatformTransactionManager transactionManager() {
+	       JpaTransactionManager transactionManager = new JpaTransactionManager();
+	       transactionManager.setEntityManagerFactory(entityManagerFactory().getObject());
+	    
+	       return transactionManager;
+	   }
+	    
+	   @Bean
+	   public PersistenceExceptionTranslationPostProcessor exceptionTranslation(){
+	       return new PersistenceExceptionTranslationPostProcessor();
+	   }
+	    
+	   Properties additionalProperties() {
+	       Properties properties = new Properties();
+	       properties.setProperty("hibernate.hbm2ddl.auto", "create");
+	       properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+	          
+	       return properties;
+	   }
+	   
+	  
+
+}
+
+
+
+
+
+
+
+
+
+
+	

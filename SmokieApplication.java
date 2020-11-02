45f3c537f6e9cc53d54e908354a0ef36e15bcdfb
Vikram Kumar <49100939+VikramKumar98@users.noreply.github.com>
2020-11-02 16:09:46 +0530

    Spring Junit

---
+++ SmokieApplication.java
@@ -0,0 +1,13 @@
+package com.example.smokie;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmokieApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmokieApplication.class, args);
+	}
+
+}

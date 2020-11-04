276fe0b6ba028080613fb9c2cd33d279afaa6b4d
sowjanya Jain <sowjanayajainnrp@gmail.com>
2020-11-04 05:38:58 -0800

    Spring security with authontication and authorization

---
+++ PasswordGenerator.java
@@ -0,0 +1,15 @@
+package com.sowjanya;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class PasswordGenerator {
+
+	public static void main(String[] args) {
+		BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+		String rawPassword = "little";
+		String encodedPassword = encoder.encode(rawPassword);
+		System.out.println(encodedPassword);
+
+	}
+
+}

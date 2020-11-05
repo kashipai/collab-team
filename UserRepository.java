dc7a20170a08948f7666190b710190c81cc87782
sneha-y-v <sneha.yv@robosoft.com>
2020-11-05 12:29:29 +0530

    Springboot security jpa mysql

---
+++ UserRepository.java
@@ -0,0 +1,9 @@
+package com.sneha.springSecurity;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Integer> {
+    Optional<User> findByUserName(String userName);
+}

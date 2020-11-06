b280eaf15f7670599265a87cbac94c6b97a41140
Soumya Shree A P <soumyasap4@gmail.com>
2020-11-06 11:15:37 +0530

    Create AssignmentSpringSecurityApplication.java

---
+++ AssignmentSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.springSecurity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AssignmentSpringSecurityApplication.class, args);
+	}
+
+}
